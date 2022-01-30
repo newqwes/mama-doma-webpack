1d73185a60b4a6d98f41ba2ef939d58aafde501b
Nazar Ashyrov <newnewqwes@gmail.com>
2022-01-30 19:45:17 +0300

    update data

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-// Важно!!! Делаем поиск по всему проекту 58temphash и заменяем на +1 к числу, заменяем везде
-// 58temphash  затем только билдим проект!!!
+// Важно!!! Делаем поиск по всему проекту 59temphash и заменяем на +1 к числу, заменяем везде
+// 59temphash  затем только билдим проект!!!
 
 //подключаем gulp
 const { src, dest, parallel, series, watch } = require('gulp');
@@ -52,7 +52,7 @@ function scripts() {
     'app/js/mask.js',
     'app/js/lib/sweetalert.min.js',
   ])
-    .pipe(concat('app58temphash.min.js'))
+    .pipe(concat('app59temphash.min.js'))
     .pipe(uglify())
     .pipe(dest('app/js/'))
     .pipe(browserSync.stream());
@@ -60,7 +60,7 @@ function scripts() {
 
 function scriptsMenu() {
   return src(['app/js/humburger.js'])
-    .pipe(concat('appMenu58temphash.min.js'))
+    .pipe(concat('appMenu59temphash.min.js'))
     .pipe(uglify())
     .pipe(dest('app/js/'))
     .pipe(browserSync.stream());
@@ -70,7 +70,7 @@ function scriptsMenu() {
 function styles() {
   return src('app/sass/style.scss')
     .pipe(sass())
-    .pipe(concat('style58temphash.min.css'))
+    .pipe(concat('style59temphash.min.css'))
     .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
     .pipe(cleanCss({ level: { 1: { specialComments: 0 } } }))
     .pipe(dest('app/css/'))
@@ -102,8 +102,8 @@ function buildcopy() {
     [
       'app/css/**/*.min.css',
       'app/**/*.php',
-      'app/js/app58temphash.min.js',
-      'app/js/appMenu58temphash.min.js',
+      'app/js/app59temphash.min.js',
+      'app/js/appMenu59temphash.min.js',
       'app/images/dest/**/*',
       'app/font/**/*',
       'app/video/**/*',
